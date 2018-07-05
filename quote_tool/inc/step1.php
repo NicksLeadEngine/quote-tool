25f4189f79da14415b9d0c869b945b1ba374f8c3
Nicholas Wan <nicholaswan@Nicholass-MacBook-Air.local>
2018-07-05 12:17:58 +0100

    Restored

--- quote_tool/inc/step1.php
+++ quote_tool/inc/step1.php
@@ -126,7 +126,7 @@ if (isset($_POST['personalTitle']))
 	echo $status_code . " " . $response;
 
 	//echo '<script type="text/javascript">window.location = "http://mortgageresponse.co.uk/quote-tool/step-2/"</script>';
-	echo '<script type="text/javascript">window.location = "'$_SERVER['HTTP_HOST'].'/quote-tool/step-2'.'"</script>';
+	echo '<script type="text/javascript">window.location = "'.$_SERVER['HTTP_HOST'].'/quote-tool/step-2'.'"</script>';
 }
 
 ?>
